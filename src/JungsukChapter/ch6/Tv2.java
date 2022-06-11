6d2f0ec7b16fbfdb0788ae6f9638c19ead56471a
Jun <98331852+Moonjuun@users.noreply.github.com>
2022-06-11 20:55:40 +0900

    Practice Java ch6, 6-4

--- src/JungsukChapter/ch6/Tv2.java
+++ src/JungsukChapter/ch6/Tv2.java
@@ -2,17 +2,6 @@ package JungsukChapter.ch6;
 // 예제6-2
 // 예제6-1꺼에서 조금 더 심화
 
-public class Tv2 {
-    //Tv의 속성(멤버변수)
-    String color;   //색상
-    boolean power;   //전원상태(on/off)
-    int channel;    //채널
-
-    //TV의 기능
-    void power() {power = !power;} // TV를 켜거나 끄는 기능의 메서드
-    void channelUp() {++channel;} // TV의 채널을 올리는 기능의 메서드
-    void channelDown() {--channel;} // TV의 채널을 내리는 기능의 메서드
-}
 
 class TvTest2 {
     public static void main(String args[]){
